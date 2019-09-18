891c24993711a92fdff10f5396b5fafb1e1b48e4
CodingRuslan <orion.hola@gmail.com>
2019-09-18 17:00:09 +0300

    feat: fix orderHandler query

--- controllers/newOrderController.js
+++ controllers/newOrderController.js
@@ -4,27 +4,13 @@ const con = require('../db');
 exports.orderHandler = (req, res, next) =>  {
     if (req.length < 1) {console.log('you should make a order'); return}
     async.waterfall([
-        function(callback) {
-            let resTime = 0;
-            con.query("SELECT * FROM ingredients\n" +
-                `where idingredients IN (${req.cartItems.join(',')})`, (err, result) => {
-                result.forEach((e) => {
-                    resTime += e.timeCook;
-                });
-                callback(null, resTime)
-            });
-
-        },
-        function (resTime, callback) {
-            let valStr ="INSERT INTO clientIngredients (orderId, ingredientId) VALUES ";
+        function (callback) {
             con.query("INSERT INTO `pizzadb`.`clientOrder` (`userId`, `cookId`, `orderDone`, `timeCooking`) " +
-                `VALUES ('${req.userId}', '1', '0', '${resTime}');`, (err, result) => {
+                `VALUES ('${req.userId}', '1', '0', '${req.timeCooking}');`, (err, result) => {
                 req.cartItems.forEach((e) => {
-                    valStr += `(${result.insertId}, ${e}),`;
+                    con.query(`INSERT INTO clientIngredients (orderId, ingredientId)  VALUES (${result.insertId}, ${e})`)
                 });
-
-                con.query(valStr.substring(0, valStr.length - 1));
-                callback(null, [result.insertId, resTime]);
+                callback(null, [result.insertId, req.timeCooking]);
             })
         }
     ],
